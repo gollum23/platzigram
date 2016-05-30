a43f861b125d8f849e4cf70d4e9fbbf03169d948
Diego Forero <gollum23@gmail.com>
2016-05-30 16:27:21 -0500

    [Update] Compile scss to css using gulp-sass

---
+++ Gulpfile.js
@@ -0,0 +1,16 @@
+/**
+ * Created by gollum23 on 30/05/16.
+ */
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+
+gulp.task('styles', function () {
+    gulp
+        .src('source/sass/index.scss')
+        .pipe(sass())
+        .pipe(rename('app.css'))
+        .pipe(gulp.dest('public/css/'));
+});
+
+gulp.task('default', ['styles']);
