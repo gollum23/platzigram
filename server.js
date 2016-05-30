360908bdc664acaa9a72ff065e75482a3c17e4bd
Diego Forero <gollum23@gmail.com>
2016-05-30 16:41:03 -0500

    [Update] Add comments

--- server.js
+++ server.js
@@ -5,14 +5,18 @@ var express = require('express');
 
 var app = express();
 
+// Define template engine
 app.set('view engine', 'pug');
 
+// Serve statics files
 app.use(express.static('public'));
 
+// Get route /
 app.get('/', function(req, res) {
     res.render('index')
 });
 
+// Define port for express server
 app.listen(3000, function(err) {
     if (err) return console.log('Hubo un error'), process.exit(1);
 
