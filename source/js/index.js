6bd347633fe4f69daec9589767883cd8b19b1ab4
Diego Forero <gollum23@gmail.com>
2016-05-30 18:12:38 -0500

    [Update] Define paths using page.js

--- source/js/index.js
+++ source/js/index.js
@@ -1,4 +1,16 @@
 /**
  * Created by gollum23 on 30/05/16.
  */
-alert('Hola');
+var page = require('page');
+
+var main = document.getElementById('main-container');
+
+page('/', function (ctx, next) {
+    main.innerHTML = 'Home'
+});
+
+page('/signup', function (ctx, next) {
+    main.innerHTML = 'Signup'
+});
+
+page();
